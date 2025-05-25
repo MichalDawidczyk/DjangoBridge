814c38636be3e5bb59942b5827eeb8b32f88b4f9
Michal Dawidczyk <michal.dawidczyk@vp.pl>
2025-05-25 21:11:26 +0200

    update /users, run django with fastAPI

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi_app.routes import router
 from fastapi_app.routes_auth import router as auth_router
+import uvicorn
+import subprocess
+
+def start_django():
+    subprocess.Popen(["python", "manage.py", "runserver"])
 
 app = FastAPI(title="DjangoBridge API Gateway")
 app.include_router(router)
 app.include_router(auth_router)
 
 if __name__ == "__main__":
-    import uvicorn
+    start_django()
     uvicorn.run(app, host="127.0.0.1", port=8001)
